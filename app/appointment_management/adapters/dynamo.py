943c1727eaaa7548152760d7de3b901652deac2f
Andres Cadena <andrescadena@MacBook-Pro-de-Andres.local>
2024-06-24 20:35:48 -0500

    Add dynamo crud  appoiment method

--- app/appointment_management/adapters/dynamo.py
+++ app/appointment_management/adapters/dynamo.py
@@ -1,7 +1,8 @@
 # from app.appointment_management.domain import ports
 
 from boto3 import resource
-from boto3.dynamodb.conditions import Attr, Key
+
+# from boto3.dynamodb.conditions import Attr, Key
 from datetime import datetime
 
 
@@ -14,6 +15,18 @@ class DynamoDbAdapter:
         response = demo_table.put_item(Item=data)
         print(f"insert response: {response}")
 
+    def modify_appointment(self, data, dynamo_table_name: str) -> None:
+        demo_table = resource("dynamodb").Table(dynamo_table_name)
+        response = demo_table.update_item(Key=data)
+        print(f"update response: {response}")
+
+    def delete_appointment(self, data, dynamo_table_name: str) -> None:
+        demo_table = resource("dynamodb").Table(dynamo_table_name)
+        response = demo_table.delete_item(Key=data)
+        print(f"delete response: {response}")
+
+    # def modify_appointment(self, model_params: ports.models.ModifyAppointmentParams) -> str:
+    # print(model_params) # model_params is a dictionary  {'customer_id': 'cus-10', 'order_id': 'ord-7', 'status': 'approved', 'created_date': '2021-09-29T14:00:00.000000'}
     # print(model_params)
 
     # def modify_appointment(self, model_params: ports.models.ModifyAppointmentParams) -> str:
@@ -30,4 +43,5 @@ data = {
     "created_date": datetime.now().isoformat(),
 }
 dynamo_conection = DynamoDbAdapter()
-dynamo_conection.create_appointment(data=data, dynamo_table_name="dynamo-python")
+# dynamo_conection.create_appointment(data=data, dynamo_table_name="dynamo-python")
+dynamo_conection.delete_appointment(data={"customer_id": "cus-10", "order_id": "ord-7"}, dynamo_table_name="dynamo-python")
